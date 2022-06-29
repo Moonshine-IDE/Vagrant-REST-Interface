02a91d7393a325c8f676667fbc103182f4e393c8
Samuel <samuel.olaechea@gmail.com>
2022-06-29 12:35:36 -0400

    External configuration file to REST-Interface
    Update vagrant script to pass a parameter for external configuration to the REST-Interface.
    
    Moonshine-IDE/REST-Interface#7

--- src/ubuntu/always.sh
+++ src/ubuntu/always.sh
@@ -4,6 +4,7 @@ HTTP_GUEST_PORT=$1
 ARTIFACT_NAME="rest-interface.jar"
 APP_HOME_DIR=$2
 HTTP_HOST_PORT=$3
+EXTERNAL_CONFIG_FILE=$4
 
 check=$(netstat -an | grep $HTTP_GUEST_PORT | wc -l)
 if [[ $check -ne 0 ]];then
@@ -14,6 +15,8 @@ fi
 cd $APP_HOME_DIR
 
 printf "\n\nStarting rest-interface..."
-java -Xmx1024m -Dgrails.env=prod -Dlogging.level.root=ERROR -Dserver.port=$HTTP_GUEST_PORT -jar bin/$ARTIFACT_NAME > log/output.log 2>&1 &
+java -Xmx1024m -Dgrails.env=prod -Dlogging.level.root=ERROR -Dserver.port=$HTTP_GUEST_PORT \
+     -Dspring.config.location=classpath:application.yml,optional:file:$EXTERNAL_CONFIG_FILE \
+     -jar bin/$ARTIFACT_NAME > log/output.log 2>&1 &
 
 printf "\n\nrest-interface is starting at 127.0.0.1:$HTTP_HOST_PORT, it takes about 20 seconds to become ready, please wait..."
